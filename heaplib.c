573cd15c169ddae977a1c66749ce458c8d51853c
Laura <ll543@cornell.edu>
2017-11-18 12:04:11 -0500

    add all files

---
+++ heaplib.c
@@ -0,0 +1,48 @@
+#include <stdlib.h>
+#include <stdio.h>
+#include "heaplib.h"
+#include <string.h>
+#include <stdbool.h>
+#include <stdint.h>
+#include <assert.h>
+
+
+/* See the .h for the advertised behavior of this library function.
+ * These comments describe the implementation, not the interface.
+ *
+ * YOUR COMMENTS GO HERE.
+ */
+void hl_init(void *heap, unsigned int heap_size) {
+
+    return;
+}
+
+/* See the .h for the advertised behavior of this library function.
+ * These comments describe the implementation, not the interface.
+ *
+ * YOUR COMMENTS GO HERE.
+ */
+void *hl_alloc(void *heap, unsigned int block_size) {
+
+    return FAILURE;
+}
+
+/* See the .h for the advertised behavior of this library function.
+ * These comments describe the implementation, not the interface.
+ *
+ * YOUR COMMENTS GO HERE.
+ */
+void hl_release(void *heap, void *block) {
+
+}
+
+/* See the .h for the advertised behavior of this library function.
+ * These comments describe the implementation, not the interface.
+ *
+ * YOUR COMMENTS GO HERE.
+ */
+void *hl_resize(void *heap, void *block, unsigned int new_size) {
+
+    return FAILURE;
+}
+
