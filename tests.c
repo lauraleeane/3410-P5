333a17ec154f90832841f3ba0c715995ce34abec
Laura <ll543@cornell.edu>
2017-11-18 13:29:44 -0500

    updated tests

--- tests.c
+++ tests.c
@@ -551,7 +551,7 @@ int test15() {
     char heap[HEAP_SIZE];
     hl_init(heap,HEAP_SIZE);
 
-    heap=8;
+    header=header+1;
 
    /*#ifdef PRINT_DEBUG
     printf("%lu heap pointer\n", (uintptr_t)&heap);
